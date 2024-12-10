6edc3ef16648336ee0a17b7a654514df82b415e8
Victoria-Rodriguez <vicrodriguezgimenez@gmail.com>
2024-12-10 13:16:43 +0100

    finished jobs routes

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -2,23 +2,163 @@
 const pool = require("../config/db");
 
 const getJobs = async (req, res) => {
-  res.send({ message: "get all jobs" });
+  try {
+    //get user id from request object
+    const userId = req.user_id;
+
+    const [result] = await pool.query("SELECT * FROM jobs WHERE user_id = ?", [
+      userId,
+    ]);
+
+    console.log("results", result);
+    res.status(200).send(result);
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to get all jobs" });
+  }
 };
 
 const getJobById = async (req, res) => {
-  res.send({ message: "get one job" });
+  try {
+    const { id } = req.params;
+
+    const userId = req.user_id;
+
+    const [job] = await pool.query(
+      "SELECT * FROM jobs WHERE id=? AND user_id=?",
+      [id, userId]
+    );
+
+    if (job.length === 0) {
+      return res.status(404).json({
+        error: "This job was not found",
+      });
+    }
+
+    res.status(200).send({ job: job[0] });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: "Failed to get job" });
+  }
 };
 
 const addJob = async (req, res) => {
-  res.send({ message: "added a job" });
+  try {
+    const userId = req.user_id;
+
+    const {
+      title,
+      company_name,
+      location,
+      location_type,
+      description,
+      type,
+      date_range,
+      min_salary,
+      max_salary,
+      has_applied,
+      created_at,
+      url,
+    } = req.body;
+
+    // title, company name, description, location, type, has_applied, and url are required
+    if (
+      !title ||
+      !company_name ||
+      !description ||
+      !location ||
+      !type ||
+      has_applied === undefined ||
+      !url
+    ) {
+      return res.status(400).json({
+        error:
+          "title, company name, description, location, type, has_applied, and url are required.",
+      });
+    }
+
+    const [result] = await pool.query(
+      "INSERT INTO jobs (user_id, title, company_name, location, location_type, description, type, date_range, min_salary, max_salary, has_applied, created_at, url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        userId,
+        title,
+        company_name,
+        location,
+        location_type || null,
+        description,
+        type,
+        date_range || null,
+        min_salary || null,
+        max_salary || null,
+        has_applied || false,
+        created_at || null,
+        url,
+      ]
+    );
+
+    res.status(201).json({
+      message: "Job was added successfully",
+      jobId: result.insertId,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Failed to add the job" });
+  }
 };
 
 const updateJob = async (req, res) => {
-  res.send({ message: "updated a job" });
+  try {
+    const { id } = req.params;
+    const userId = req.user_id;
+    const { has_applied } = req.body;
+
+    //check if it is provided
+    if (has_applied === undefined) {
+      return res.status(400).json({
+        error: "The has_applied field is required",
+      });
+    }
+
+    const [result] = await pool.query(
+      "UPDATE jobs SET has_applied=? WHERE id=? AND user_id=?",
+      [has_applied, id, userId]
+    );
+
+    //check if rows have been affected
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "This job was not found" });
+    }
+
+    res.status(200).send({ message: "has_applied status has been updated" });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({
+      error: "Failed to update has_applied status",
+    });
+  }
 };
 
 const deleteJob = async (req, res) => {
-  res.send({ message: "deleted a job" });
+  try {
+    const { id } = req.params;
+    const userId = req.user_id;
+
+    const [result] = await pool.query(
+      "DELETE FROM jobs WHERE id=? AND user_id=?",
+      [id, userId]
+    );
+
+    //check if the job was deleted
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "This job was not found." });
+    }
+
+    res.status(200).send({ message: "Job has been deleted." });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({
+      error: "Failed to delete job.",
+    });
+  }
 };
 
 module.exports = {
--- server/routes/jobsRoutes.js
+++ server/routes/jobsRoutes.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
+require("dotenv").config();
+const userShouldBeLoggedIn = require("../guard/userShouldBeLoggedIn");
 
 const jobsController = require("../controllers/jobsController");
 
 //any route in this file is pre-pended with /api/jobs
 
-router.get("/", jobsController.getJobs); //fetches all jobs
+router.get("/", userShouldBeLoggedIn, jobsController.getJobs); //fetches all jobs for a user
 
-router.get("/:id", jobsController.getJobById); //fetches a job by ID
+router.get("/:id", userShouldBeLoggedIn, jobsController.getJobById); //fetches a job by ID
 
-router.post("/", jobsController.addJob); //adds new job
+router.post("/", userShouldBeLoggedIn, jobsController.addJob); //adds a new job
 
-router.put("/:id", jobsController.updateJob); //updates a job
+router.put("/:id/has_applied", userShouldBeLoggedIn, jobsController.updateJob); //updates has_aplied column
 
-router.delete("/:id", jobsController.deleteJob); //deletes a job
+router.delete("/:id", userShouldBeLoggedIn, jobsController.deleteJob); //deletes a job
 
 module.exports = router;
