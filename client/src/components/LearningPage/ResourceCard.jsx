1dafdec30827663f31315c9147db173940439b7c
Mara Caldarini <mara.caldarini@gmail.com>
2025-01-04 13:53:06 +0000

    fixed flexbox and added href

--- client/src/components/LearningPage/ResourceCard.jsx
+++ client/src/components/LearningPage/ResourceCard.jsx
@@ -3,16 +3,20 @@ import "./ResourceCard.css";
 export const ResourceCard = ({ r }) => {
   return (
     <div className="resource-card">
-      <div className="r-title">
-        <h4>{r.resource_title}</h4>
-      </div>
-      <div className="r-type">{r.type}</div>
-      <div className="r-discipline">{r.discipline}</div>
-      <div className="r-skill">
-        {r.skills.map((skill, index) => (
-          <div key={index}>{skill.skills_title}</div>
-        ))}
-      </div>
+      <a href={r.url} target="_blank">
+        <div className="r-type">{r.type}</div>
+        <div className="r-title">
+          <h4>{r.resource_title}</h4>
+        </div>
+        <div className="r-type">{r.type}</div>
+
+        <div className="r-discipline">{r.discipline}</div>
+        <div className="r-skill">
+          {r.skills.map((skill, index) => (
+            <div key={index}>{skill.skills_title}</div>
+          ))}
+        </div>
+      </a>
     </div>
   );
 };
