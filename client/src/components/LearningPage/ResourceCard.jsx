1ce0067d4f4caf0c83ad3cfc94eff26986759057
Mara Caldarini <mara.caldarini@gmail.com>
2025-01-04 17:17:02 +0000

    fixed layout

--- client/src/components/LearningPage/ResourceCard.jsx
+++ client/src/components/LearningPage/ResourceCard.jsx
@@ -2,21 +2,35 @@ import "./ResourceCard.css";
 
 export const ResourceCard = ({ r }) => {
   return (
-    <div className="resource-card">
-      <a href={r.url} target="_blank">
-        <div className="r-type">{r.type}</div>
-        <div className="r-title">
-          <h4>{r.resource_title}</h4>
+    <a href={r.url} target="_blank">
+      <div className="resource-card">
+        <div className="r-top-container">
+          <div
+            className={
+              r.type === "course"
+                ? "r-type course"
+                : r.type === "tutorial"
+                ? "r-type tutorial"
+                : "r-type book"
+            }
+          >
+            {r.type}
+          </div>
+          <div className="r-title">
+            <h4>{r.resource_title}</h4>
+          </div>
         </div>
-        <div className="r-type">{r.type}</div>
-
-        <div className="r-discipline">{r.discipline}</div>
-        <div className="r-skill">
-          {r.skills.map((skill, index) => (
-            <div key={index}>{skill.skills_title}</div>
-          ))}
+        <div className="r-bottom-container">
+          <div className="r-skills">
+            {r.skills.map((skill, index) => (
+              <div className="skill-badge" key={index}>
+                {skill.skills_title}
+              </div>
+            ))}
+          </div>
+          <div className="r-discipline">{r.discipline}</div>
         </div>
-      </a>
-    </div>
+      </div>
+    </a>
   );
 };
