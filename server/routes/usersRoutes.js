29427b7417fc0fef760c73540e4319837c457206
Mara Caldarini <mara.caldarini@gmail.com>
2025-04-04 16:41:14 +0100

    fixed comments in usersRoutes.js

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -13,27 +13,29 @@ const {
   deleteUser,
 } = require("../controllers/usersController");
 
-// add a route to get all users: GET /
+//any route in this file is pre-pended with /api/users
+
+//route to get all users: GET /
 router.get("/", getAllUsers);
 
-// add a route to get all skills by user id: GET /
+//route to get all skills by user id: GET /
 /* router.get("/:id/skills", userShouldBeLoggedIn, getSkillsByUserId); */
 
 router.get("/skills", userShouldBeLoggedIn, getSkillsByUserId);
 
-// add a route to post skills by user: POST /
+//route to post skills by user: POST /
 router.post("/addSkill", userShouldBeLoggedIn, addSkillToUser);
 
-// add a route to register a user: POST /
+//route to register a user: POST /
 router.post("/", registerUser);
 
-// add a route to register a user: POST /
+//route to register a user: POST /
 router.post("/login", login);
 
-// add a route to add profile from user: GET /
+//route to add profile from user: GET /
 router.get("/profile", userShouldBeLoggedIn, getProfile);
 
-//add a route to delete a user: DELETE /
+//route to delete a user: DELETE /
 router.delete("/:id", deleteUser);
 
 module.exports = router;
